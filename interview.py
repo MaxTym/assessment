2fa1fa091901d9c1baae3b90f8ad0fb3e9d1a54d
Maksym Tymtsiv <mtymtsiv@gmail.com>
2017-03-23 11:32:09 -0400

    comments added

--- interview.py
+++ interview.py
@@ -1,3 +1,5 @@
+# Function that takes string as input.
+# Output to be int/float/string depending if the string can be converted to float or integer type
 def q2(item):
     try:
         return type(int(item))
@@ -12,7 +14,8 @@ def q3():
     abc = ['dog', 'Fido', 10]
     return '{} the {} is {} years old'.format(abc[1], abc[0], abc[2])
 
-
+# method that takes 3 numbers as input and finds the minimum of the three
+# without using the built- in min function
 def q4(a, b, c):
    l = sorted([a, b, c])
    return l[0]
